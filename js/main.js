a87cb3eb261988724aaa8ad65e65e8ce7e9a016f
DuvanCamiloArenasRodriguez <dccar21@gmail.com>
2024-02-03 23:40:07 -0500

    styles: :lipstick: Dialog Box of "Producto 1"
    Dialog Box was added for "Producto 1". Additionally, some mistakes in the distribution of Web Page and Responsive Designe were fixed
    
    @CampusLands

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ document.querySelector("#btnMenu").addEventListener("click", function(){
     menu.classList.toggle("active")
     // console.log("jhjdfh")
 })
+
+window.show.addEventListener('click', () => {
+    window.dialog.showModal()
+    console.log("asfsdsvdfb")
+})
+
+window.hide.addEventListener('click', () => {
+    window.dialog.close()
+})
